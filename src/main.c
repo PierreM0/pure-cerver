f632dd08a2e48697648c3e2be9f9e0f0432bd43b
PierreM0 <pierreemmanuel.devin@posteo.net>
2023-09-26 20:17:28 +0200

    now with routing !

--- src/main.c
+++ src/main.c
@@ -3,6 +3,7 @@
 #define EXIT_SUCCESS 0
 #define EXIT_FAILURE 1
 
+#define BUFSIZ 8192
 
 typedef unsigned short uint16_t;
 typedef unsigned int uint32_t;
@@ -10,9 +11,12 @@ typedef unsigned int size_t;
 typedef unsigned int socklen_t;
 typedef int ssize_t;
 
+#define O_RDONLY 00
+#define S_IRUSR 0400
+
 #define AF_INET 2
 #define SOCK_STREAM 1
-#define IPPROTO_TCP 0
+#define IPPROTO_TCP 0 
 #define INADDR_ANY 0
 
 typedef struct {
@@ -22,8 +26,15 @@ typedef struct {
   char sin_zero[8];    /* 16 */
 } sockaddr_in_t;
 
+typedef struct {
+  uint16_t sa_family_t;
+  char padding[14];
+} sockaddr_t;
 
+
+int read(int fd, void* buf, int buflen);
 int write(int fd, void* buf, int buflen);
+int open(void* pathname, int flags, int mode);
 int close(int fd);
 int socket(int domain, int type, int protocol);
 int accept(int sockfd, void* addr, int* addrlen);
@@ -33,6 +44,7 @@ int exit(int code);
 
 #define err_log(x) do {write(STDERR, "ERROR: "x, strlen(x) + 7); exit(EXIT_FAILURE); } while(0);
 #define info_log(x) write(STDOUT, "INFO: "x, strlen(x) + 6) 
+#define warn_log(x) write(STDOUT, "WARN: "x, strlen(x) + 6) 
 
 
 int strlen(const char* str) {
@@ -52,6 +64,39 @@ int strcat(char* DEST, char* SRC) {
 }
 
 
+/* return  the route in char* route
+ * return the size of the route
+ * */
+int parse_route(char* header, char* route) {
+  /* GET /index.html HTTP/1.1 */
+
+  /* goto first space */
+
+  int header_ptr;
+  int route_ptr;
+
+  header_ptr = 0;
+  route_ptr = 0;
+
+  while (header[header_ptr] != '/') ++header_ptr;
+  header_ptr += 1;
+  /* copy the route */
+
+  while (header[header_ptr] != ' ') {
+    route[route_ptr] = header[header_ptr];
+    header_ptr += 1;
+    route_ptr += 1;
+  }
+
+  if (route_ptr == 0) {
+    route[route_ptr] = '\0'; 
+    route_ptr = strcat(route, "index.html");
+  }
+
+  route[route_ptr] = '\0'; 
+  return route_ptr;
+}
+
 
 int main(int argc, char* argv[]) {
   int i = 0;
@@ -65,10 +110,10 @@ int main(int argc, char* argv[]) {
   }
 
   info_log("Creating socket...\n");
-  if ((sockfd = socket(AF_INET, SOCK_STREAM, IPPROTO_TCP)) < 0) {
+  if ((sockfd = socket(AF_INET, SOCK_STREAM,IPPROTO_TCP)) < 0) {
 	err_log("socket\n");
   }
-  #define EXIT_SUCCESS 0
+  
 
 
   info_log("Binding socket...\n");
@@ -79,29 +124,58 @@ int main(int argc, char* argv[]) {
   if (bind(sockfd, &addr, sizeof(addr)) < 0) {
       err_log("bind");
   }
+  
 
   info_log("Listening socket...\n");
   if (listen(sockfd, 5) < 0) {
       err_log("listen");
   }
+  
 
   for(;;) {
-    int addr_out_len; sockaddr_in_t SA;
+    int addr_out_len;
+    sockaddr_t SA;
+    char header[BUFSIZ];
+    int header_len;
+    #define MAX_ROUTE 255
+    char route[MAX_ROUTE];    
+
+    int html_fd;
+    char html[BUFSIZ];
+    int html_len;
+
     info_log("Accepting client...\n");
     confd = accept(sockfd, &SA, &addr_out_len);
-    (void ) addr_out_len;
-    (void ) SA;
-    if ( confd < 0 ) err_log("accept\n");
+    if ( confd < 0 )err_log("accept\n");
 
-    char html[] = "HTTP/1.1 200 OK\r\n"
+
+    if ((header_len = read(confd, header, BUFSIZ)) < 0) 
+      err_log("read");
+
+
+    parse_route(header, route);
+
+    if ((html_fd = open(route, O_RDONLY, S_IRUSR)) < 0) {
+          char header[] = "HTTP/1.1 404 ERROR\r\n";
+          write(confd, header, strlen(header));
+          warn_log("404 error\n");
+          write(STDOUT, route, strlen(route));
+    } else {
+      
+      if ((html_len = read(html_fd, html, BUFSIZ)) < 0)
+        err_log("read");
+
+
+      char header[] = 
+          "HTTP/1.1 200 OK\r\n"
        		"Content-Type: text/html; charset=utf-8\r\n"
-       		"\r\n"
-  		"<h1>test</h1>\r\n";
-    write(confd, html, strlen(html));
+       		"\r\n";
+
+      write(confd, header, strlen(header));
+      write(confd, html, html_len);
+    }
     close(confd);
   } 
   return EXIT_SUCCESS;
 }
-
-
 
